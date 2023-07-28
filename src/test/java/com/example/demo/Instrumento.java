180ecc6f861e4909cbf2468f4546a9e5927f195d
zurieldelacruz <128268732+zurieldelacruz@users.noreply.github.com>
2023-07-28 04:44:33 +0000

    creando la conexion postgree

---
+++ src/test/java/com/example/demo/Instrumento.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="tbl_instrumento")
+public class Instrumento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String nombre;
+    private Integer id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_Categoria")
+    private Categoria categoria;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+}
